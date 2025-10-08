ccd2009537dd2cdb42857ef0863ffa10a39d5eb9
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-10-08 00:02:40 +0000

    add the room socket

---
+++ src/sockets/room.ts
@@ -0,0 +1,31 @@
+import { Server, Socket } from "socket.io";
+import Room from "../models/roomModel";
+
+
+export function roomSocket(io:Server, socket:Socket){
+    socket.on("join room", async({ roomId}, cb) => {
+        const room = await Room.findOne({ id:roomId})
+        if(!room){
+            cb?.({ ok:false, error:"invalid room"})
+        }
+        if(!room?.members.includes(socket.data.id)){
+            room?.members.push(socket.data.id)
+        }
+
+        socket.join(roomId)
+        cb?.({ok:true})
+        io.to(roomId).emit("player joined", room?.members)
+    })
+
+
+    socket.on("leave room", async({ roomId }, cb) => {
+        const room = await Room.updateOne({ id:roomId}, { $pull: { members: socket.data.id}})
+        if(!room){
+            cb?.({ok:false, error:"invalid roomId"})
+        }
+
+        socket.leave(socket.data.id)
+        io.to(roomId).emit("playerLeft", socket.data.userId);
+        cb?.({ ok: true });
+    })
+}

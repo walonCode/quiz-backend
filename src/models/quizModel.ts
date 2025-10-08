8a117ed254a5fbc8fff0a0b70a9439e8ef01bffc
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-10-08 07:08:02 +0000

    add the quiz model

---
+++ src/models/quizModel.ts
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from "mongoose";
+
+const quiz = new Schema({
+    roomId:{ type:mongoose.Schema.Types.ObjectId, ref:"Room", required:true},
+    startTime:{ type:Date, required:true},
+    endTIme: { type:Date },
+    highScore: { 
+        userId: { type: mongoose.Schema.Types.ObjectId, ref:"User"},
+        score: { type:Number, default:0}
+    }
+}, { timestamps:true })
+
+const Quiz = mongoose.model("quiz", quiz)
+
+export default Quiz;
